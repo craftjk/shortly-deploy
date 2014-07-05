9da36ab8c73a2b02c15e7ff6613bcc95810be9e6
Hack Reactor Students <accounts@hackreactor.com>
2014-07-05 16:35:08 -0700

    Concatenates client js files with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      dist: {
+        src: ['client/*.js'],
+        dest: 'public/client/production.js',
+      },
     },
 
     mochaTest: {
